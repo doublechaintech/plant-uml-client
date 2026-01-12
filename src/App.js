7580bac17350df90231bed505374be3d89fe4e24
Philip Zhang <zhangxilai@doublechaintech.com>
2026-01-12 17:43:13 +0800

    🚀save for delete

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import './App.css';
 import SplitPane, { Pane } from 'react-split-pane';
 import { useEffect, useState } from 'react';
 import { Buffer } from 'buffer';
-import Editor from 'react-simple-code-editor';
 import deflate from 'deflate-js'
 
-import { highlight, languages } from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/themes/prism.css'; //Example style, you can use another
-
+import TextEditor from 'react-local-store-left-text-pane'
 
 const BASE_64_MAP="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 const PUML_64_MAP="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_"
@@ -39,58 +34,13 @@ const getURL=(value)=>{
 
 function App() {
 
-  const [text,setText]=useState(`@startuml
-  |金蝶系统|
-  |#lightblue|CMS集成模块|
-  |**CMS后台系统**|
-  
-  start
-  if (配送瓶气订单) then (是)
-     
-    |司机@App|
-    :厂外卸货/回收空瓶;
-    |CMS后台系统|
-    :推送厂外卸货和回收空瓶事件;
-    |CMS集成模块|
-    :根据订单类型不同推送单据;
-    |金蝶系统|
-    :接收不同单据;
-    stop
-    |库管@App|
-    start
-    
-    :**入库操作**;
-    |CMS后台系统|
-    :推送厂外卸货和回收空瓶事件;
-    |CMS集成模块|
-    :根据订单类型不同推送单据;
-    |金蝶系统|
-    :接收不同单据;
-    stop
-  endif
-  stop
-  @enduml
-  
-  https://plantuml.com/zh/
-
-  
-  `)
-
+  const [text,setText]=useState("")
+  //onDidChangeModelContent
   return (
     <SplitPane split="vertical" minSize={200} defaultSize={400}>
   <div >
 
-  <Editor
-      value={text}
-      onValueChange={code => setText(code)}
-      highlight={code => highlight(code, languages.js)}
-      padding={10}
-      style={{
-        fontFamily: '"Fira code", "Fira Mono", monospace',
-        fontSize: 15, 
-        height:"90vh"
-      }}
-    />
+  <TextEditor text={text} setText={setText}/>
 
   </div>
   <div >    
@@ -127,6 +77,6 @@ function App() {
     </>
   );
 }
-
+groupName
 
 */
